6c42e258d353ed7715920293b00f360c165380eb
hammad <hammadmansuri@gmail.com>
2017-03-27 09:32:55 +0530

    Update mainController.js

--- Scripts/mainController.js
+++ Scripts/mainController.js
@@ -2,12 +2,12 @@ movieRatingApp.controller('mainController', ['$scope', '$http', '$mdToast', func
     google.charts.setOnLoadCallback(drawChart);
 
     $scope.movies = [
-        { "$id": "1", "title": "Gone Girl", "image": "images/movie1.jpg", "rating": "3", "releaseDate": "2014-02-22" },
-        { "$id": "2", "title": "The Good Life", "image": "images/movie2.jpg", "rating": "4", "releaseDate": "2014-06-24" },
-        { "$id": "3", "title": "The Hero of Color City", "image": "images/movie3.jpg", "rating": "0", "releaseDate": "2014-11-23" },
-        { "$id": "4", "title": "Guardians of the Galaxy", "image": "images/movie4.jpg", "rating": "5", "releaseDate": "2014-07-01" },
-        { "$id": "5", "title": "The Drop", "image": "images/movie5.jpg", "rating": "0", "releaseDate": "2014-12-01" },
-        { "$id": "6", "title": "If I Stay", "image": "images/movie6.jpg", "rating": "0", "releaseDate": "2018-01-01" }
+        { "$id": "1", "title": "Gone Girl", "image": "Images/movie1.jpg", "rating": "3", "releaseDate": "2014-02-22" },
+        { "$id": "2", "title": "The Good Life", "image": "Images/movie2.jpg", "rating": "4", "releaseDate": "2014-06-24" },
+        { "$id": "3", "title": "The Hero of Color City", "image": "Images/movie3.jpg", "rating": "0", "releaseDate": "2014-11-23" },
+        { "$id": "4", "title": "Guardians of the Galaxy", "image": "Images/movie4.jpg", "rating": "5", "releaseDate": "2014-07-01" },
+        { "$id": "5", "title": "The Drop", "image": "Images/movie5.jpg", "rating": "0", "releaseDate": "2014-12-01" },
+        { "$id": "6", "title": "If I Stay", "image": "Images/movie6.jpg", "rating": "0", "releaseDate": "2018-01-01" }
     ];
 
     $scope.sortOptions = [
